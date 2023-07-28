46163f55edee2d8df7f6db038a3f9c1a8b8b7433
Soham Patil <patilsoham390@gmail.com>
2023-07-28 23:29:57 +0530

    vercel deployed final

--- todo/settings.py
+++ todo/settings.py
@@ -134,6 +134,6 @@ STATICFILES_DIRS = [
 
 
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static')
     
     
